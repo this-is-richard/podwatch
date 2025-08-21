acbf459342e87016e9e490e8b74089123ae2e6ba
Richard Ng <richard.ng.cccc@gmail.com>
2025-08-21 16:42:36 +0800

    tailwindcss

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,8 @@
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.body);
-root.render(<h2>Hello from React!</h2>);
+root.render(
+  <h2 className="text-3xl font-bold underline text-red-500">
+    Hello from React!
+  </h2>
+);
--- src/renderer.ts
+++ src/renderer.ts
@@ -26,7 +26,7 @@
  * ```
  */
 
-import "./index.css";
+import "./output.css";
 
 console.log(
   '👋 This message is being logged by "renderer.js", included via webpack'
